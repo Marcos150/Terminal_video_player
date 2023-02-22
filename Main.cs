bfe90f266b9fcb1b748cea937c28fb5bc7b24dfb
Marcos Ruiz Rubio <marcosruizrubio@gmail.com>
2023-02-22 09:57:35 +0100

    Translated the code from spanish to english, part 2

--- Main.cs
+++ Main.cs
@@ -6,48 +6,42 @@ public class Principal
     static void Main()
     {
         char opt;
-        string video, duracion, text = "";
-        int total, hr, min, sec, cantidadFrames;
+        string video, duration, text = "";
+        int total, hr, min, sec, frameQuantity;
 
-        Console.WriteLine("Escribe el nombre del vídeo con extensión");
+        Console.WriteLine("Write the video file name");
         video = Console.ReadLine();
 
-        /*Process getduracion = new Process();
-        getduracion.StartInfo.FileName = "ffprobe";
-        getduracion.StartInfo.Argument = "-i " + video + " -show_entries format=duration -v quiet -of csv=\"p=0\"";
-        getduracion.StartInfo.UseShellExecute = false;
-        getduracion.StartInfo.RedirectStandardOutput = true;
-        getduracion.Start();*/
-
-        Console.WriteLine("Escribe la duración en segundos");
+        Console.WriteLine("Write the length in seconds");
         total = Convert.ToInt32(Console.ReadLine());
 
-        //Ajusta la duración del video y calcula frames totales a 30 fps
+        //Adjust the video length and calculates total frames at 30 fps
         hr = (int)total / 3600;
         min = (int)(total % 3600) / 60;
         sec = (int)(total % 3600) % 60;
-        duracion = hr + ":" + min + ":" + sec;
-        cantidadFrames = (int)total * 30;
+        duration = hr + ":" + min + ":" + sec;
+        frameQuantity = (int)total * 30;
 
-        //Genera texto con todos los frames a renderizar
-        for (int i = 1; i <= cantidadFrames; i++)
+        //Generates text with all frames to render
+        for (int i = 1; i <= frameQuantity; i++)
         {
             text = text + ("frame" + (i.ToString("D5")) + ".jpeg ");  
         }
 
-        //Extracción de frames
-        Process frames = Process.Start("ffmpeg", "-ss 00:00:00 -t " + duracion + " -i " + video + " -qscale:v 2 -r 30.0 frame%5d.jpeg");
+        //Extracts the frames
+        Process frames = Process.Start("ffmpeg", "-ss 00:00:00 -t " + duration + " -i " + video + " -qscale:v 2 -r 30.0 frame%5d.jpeg");
         frames.WaitForExit();
-        //Renderiza video con los frames
+        //Renders the video with all the frames extracted
         Process render = Process.Start("jp2a", text + "--colors -i");
         render.WaitForExit();
 
-        Console.WriteLine("¿Quieres borrar todos los frames generados [S/N]?");
+        Console.WriteLine("Do you want to remove all the files generated? [Y/N]");
         opt = Convert.ToChar(Console.ReadLine());
 
-        if (opt == 'S')
+        if (opt == 'Y')
         {
-            Process rm = Process.Start("rm", text); //borra todos los frames generados
+            //Removes all the frames generated
+            Process rm = Process.Start("rm", text);
             rm.WaitForExit();
             Console.WriteLine("Frames eliminados");
         }
